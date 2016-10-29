8b146a774121c15058eb725ce5cc12178d6a9de6
Nijam <nijam96@yahoo.co.in>
2016-10-29 15:59:37 +0100

    Added bookings schema

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class bookings(mode.Model):
+	booking_ref = models.CharField()
+	room_id     = models.IntegerField()
+	date        = models.DateField()
+	start_time  = models.TimeField()
+	end_time    = models.TimeField()
+	duration    = models.DurationField()
