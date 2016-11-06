bc472cb34269a9f728960b9dde63e966e6a5dae7
Nijamudeen <nijam96@yahoo.co.in>
2016-11-06 19:45:15 +0000

    room_detailss page

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import  url
 from . import views
 
-urlpatterns = [ url(r'^$',views.index, name = 'index')]
+urlpatterns = [ url(r'^$',views.index, name = 'index'),
+                url(r'^find_rooms/$',views.find_rooms, name = 'find_rooms'),
+                url(r'^view_room/([0-9]+)/$',views.view_room, name = 'view_room'),
+              ]
--- webapp/views.py
+++ webapp/views.py
@@ -2,12 +2,32 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.db import models
+from django.views.decorators.csrf import csrf_exempt
 from .models import rooms,bookings
 import time
 
 def index(request):
-	res = rooms.objects.raw("SELECT * FROM webapp_rooms WHERE room_id NOT IN (SELECT room_id FROM webapp_bookings WHERE date = '2016-11-01' AND start_time > '12:50' AND start_time < '13:00')")
-	print (len(list(res)))
 	cDate = time.strftime("%d-%m-%Y")
 	cTime = time.strftime("%H:%M")
-	return render_to_response('home.html', {'query_results': res, 'cDate':cDate, 'cTime':cTime})
+	res = queryDB(cDate,cTime)
+	return render_to_response('home.html',res)
+
+def queryDB(date,time):
+	res = rooms.objects.raw("SELECT * FROM webapp_rooms WHERE room_id NOT IN (SELECT room_id FROM webapp_bookings WHERE date = '2016-11-01' AND start_time > '12:50' AND start_time < '13:00')")
+	obj = {'query_results': res, 'date':date, 'time':time}
+	return obj
+
+@csrf_exempt
+def find_rooms(request):
+	date = request.POST['date']
+	time = request.POST['start_time']
+	res = queryDB(date,time)
+	return render_to_response('home.html',res)
+
+def queryRoom(id):
+	res = rooms.objects.raw("SELECT * FROM webapp_rooms WHERE room_id = %s",[id])
+	return res
+
+def view_room(request,id):
+	return render_to_response('room_details.html',{"room_details":queryRoom(id)})
+	
