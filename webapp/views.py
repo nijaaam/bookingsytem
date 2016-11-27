cb82cbb6971dab3c940c655cffe34a7786988e06
Nijamudeen <nijam96@yahoo.co.in>
2016-11-27 10:47:44 +0000

    AVAIL

--- webapp/views.py
+++ webapp/views.py
@@ -15,103 +15,24 @@ def index(request):
 		cTime = request.session['bk_time']  
 	except KeyError:
 		cTime = time.strftime("%H:%M")
-		cDate = time.strftime("%d-%m-%Y")
+		cDate = time.strftime("%Y-%m-%d")
 	res = queryDB(cDate,cTime,request)
 	return render(request,'home.html',res)
 
 def find_rooms(request):
 	cTime = time.strftime("%H:%M")
 	bk_date = request.POST['bk_date']
-	bk_time = request.POST['bk_time']
-	if time < cTime:
+	bk_time = request.POST['bk_time'] 	 	
+	if bk_time < cTime:
 		bk_time = cTime
 	res = queryDB(bk_date,bk_time,request)
 	return render(request,'home.html',res)
 
-def viewBooking(request):
-	return render(request,'viewBooking.html',{})
-
-def showWeek(request):
-	dt = datetime.now()
-	dt_str = dt.strftime("%Y-%m-%d")
-	start = dt - timedelta(days = dt.weekday())
-	end = start + timedelta(days=6)
-	start1	 = str(start.strftime("%d-%m-%y"))
-	start = str(start.strftime("%Y-%m-%d"))
-	request.session['weekNo'] = start
-	end = str(end.strftime("%Y-%m-%d"))
-	res = rooms.objects.raw("SELECT * FROM webapp_bookings WHERE room_id = %s AND date > %s AND date < %s",[request.session['bk_rm_id'],start,end])
-	return render_to_response('events_table.html',{"query_results":res,"weekNo":start1})
-
-def getBookingsforWeek(request,x):
-	dt = request.session['weekNo']
-	dt = datetime.strptime(dt,"%Y-%m-%d")
-	if x == "-":
-	    start = dt - timedelta(days = 7)
-	elif x == "+":
-		start = dt + timedelta(days = 7)
-	end = start + timedelta(days=6)
-	start1	 = str(start.strftime("%d-%m-%y"))
-	start = str(start.strftime("%Y-%m-%d"))
-	request.session['weekNo'] = start
-	end = str(end.strftime("%Y-%m-%d"))
-	#res1 = bookings.objects.filter(room_id=request.session['bk_rm_id']).filter(date__end__lte = 0).filter(date__start__gte = 0) 
-	res = rooms.objects.raw("SELECT * FROM webapp_bookings WHERE room_id = %s AND date > %s AND date < %s",[request.session['bk_rm_id'],start,end])
-	#print list(res1),list(res)
-	return start1,res
-
-def prevWeek(request):
-	(weekNo,res) = getBookingsforWeek(request,"-")
-	return render_to_response('events_table.html',{"query_results":res,"weekNo":weekNo})
-
-def nextWeek(request):
-	(weekNo,res) = getBookingsforWeek(request,"+")
-	return render_to_response('events_table.html',{"query_results":res,"weekNo":weekNo})
-
-def queryDB(date,time,request):
-	request.session['bk_date'] = date
-	request.session['bk_time'] = time
-	print date, time
-	res = rooms.objects.raw("SELECT * FROM webapp_rooms WHERE room_id NOT IN (SELECT room_id FROM webapp_bookings WHERE date = '2016-11-08' AND start_time < '19:12' AND end_time < '19:27')")
-	obj = {'query_results': res, 'bk_date':date, 'bk_time':time}
-	return obj
-
-def getMinutes(request):
-	radio = request.POST.getlist("duration_radio")
-	if radio[0] == "quarter":
-	    return "00:15"
-	elif radio[0] == "half":
-		return "00:30"
-	elif radio[0] == "threequarter":
-		return "00:45"
-	elif radio[0] == "onehour":
-		return "01:00"
-	elif radio[0] == "userDuration":
-		return request.POST['durValue'] 
-
-def convertMinutes(request):
-	minutes = getMinutes(request)
-	hours = minutes[0] + minutes[1]
-	hours = int(hours)
-	minutes1 = minutes[3]+ minutes[4]
-	min = int(minutes1) + 60*hours
-	return minutes,min
-
-def findBooking(request):
-	res = rooms.objects.raw("SELECT * FROM webapp_bookings,webapp_rooms WHERE booking_ref = %s AND webapp_bookings.room_id = webapp_rooms.room_id;",[request.POST['booking_id']]);
-	start = list(res)[0].start_time.strftime("%H:%M")
-	end = list(res)[0].end_time.strftime("%H:%M")
-	duration = "end - start"
-	start_arr = start.split(":")
-	hours = int(end.split(":")[0]) - int(start.split(":")[0])
-	minutes = int(end.split(":")[1]) - int(start.split(":")[1])
-	if minutes < 0:
-		hours = hours - 1
-		minutes = 60 + minutes
-	elif minutes == 0:
-		minutes = "00"
-	duration = str(hours) + ":" + str(minutes)
-	return render(request,'showResult.html',{"query_results":res,"duration":duration})
+def view_room(request,id):
+	request.session['bk_rm_id'] = id
+	query = rooms.objects.filter(room_id=id)
+	res = {"room_details":query}
+	return render(request,'room_details.html',res)
 
 def book_room(request):
 	contact	     = request.POST['contact']
@@ -135,18 +56,53 @@ def book_room(request):
 		"duration":minutes,
 		})
 
-def view_room(request,id):
-	request.session['bk_rm_id'] = id
-	query = rooms.objects.raw("SELECT * FROM webapp_rooms WHERE room_id = %s",[id])
-	res = {"room_details":query}
-	return render(request,'room_details.html',res)
+def viewBooking(request):
+	return render(request,'viewBooking.html',{})
+
+def showWeek(request):
+	dt = datetime.now()
+	dt_str = dt.strftime("%Y-%m-%d")
+	room_id = request.session['bk_rm_id']
+	start = dt - timedelta(days = dt.weekday())
+	end = start + timedelta(days=6)
+	start1	 = str(start.strftime("%d-%m-%y"))
+	start = str(start.strftime("%Y-%m-%d"))
+	request.session['weekNo'] = start
+	end = str(end.strftime("%Y-%m-%d"))
+	res =  bookings.objects.filter(room_id=room_id,date__range=[start,end]) 
+	return render_to_response('events_table.html',{"query_results":res,"weekNo":start1})
+
+def prevWeek(request):
+	(weekNo,res) = getBookingsforWeek(request,"-")
+	return render_to_response('events_table.html',{"query_results":res,"weekNo":weekNo})
+
+def nextWeek(request):
+	(weekNo,res) = getBookingsforWeek(request,"+")
+	return render_to_response('events_table.html',{"query_results":res,"weekNo":weekNo})
+
+def findBooking(request):
+	booking_id = request.POST['booking_id']
+	booking = bookings.objects.get(booking_ref=booking_id)
+	room = rooms.objects.get(room_id=booking.room_id)
+	start = booking.start_time.strftime("%H:%M")
+	end = booking.end_time.strftime("%H:%M")
+	duration = convertDuration(start,end)
+	return render(request,'showResult.html',{
+		"room_name": room.room_name,
+		"room_size": room.room_size,
+		"room_location": room.room_location,
+		"room_features": room.room_features,
+		"contact": booking.contact,
+		"description": booking.description,
+		"duration": duration,
+	})
 
 def updateBooking(request):
 	booking_id = request.POST['booking_id']
 	res = bookings.objects.filter(booking_ref=booking_id)
 	description = request.POST['description']
 	contact = request.POST['contact']
-	duration =""
+	duration = ""
 	room_name = " KJLKJ KL"
 	if description != list(res)[0].description:
 		bookings.objects.filter(booking_ref=booking_id).update(description=description)
@@ -159,9 +115,85 @@ def updateBooking(request):
 		"contact": contact,
 		"duration": duration,
 		})
-	
 
 def cancelBooking(request):
 	booking_id = request.POST['booking_id']
 	bookings.objects.filter(booking_ref=booking_id).delete()
 	return HttpResponse("Booking Canceled")
+
+def getBookingsforWeek(request,x):
+	dt = request.session['weekNo']
+	room_id = request.session['bk_rm_id']
+	dt = datetime.strptime(dt,"%Y-%m-%d")
+	if x == "-":
+	    start = dt - timedelta(days = 7)
+	elif x == "+":
+		start = dt + timedelta(days = 7)
+	end = start + timedelta(days=6)
+	start1	 = str(start.strftime("%d-%m-%y"))
+	start = str(start.strftime("%Y-%m-%d"))
+	request.session['weekNo'] = start
+	end = str(end.strftime("%Y-%m-%d"))
+	res = bookings.objects.filter(room_id=room_id,date__range=[start,end]) 
+	return start1,res
+
+def calculateDifference(now,start):
+	start = start.strftime("%H:%M")
+	start = datetime.strptime(start,"%H:%M")
+	now = datetime.strptime(now,"%H:%M")
+	result = str(start - now)
+	result = datetime.strptime(result,"%H:%M:%S")
+	return result.strftime("%H:%M")
+
+def getAvaliabilityTime(id,date,time):
+	upcomingBookings = bookings.objects.filter(room_id=id,date=date,start_time__gt=time).order_by('-start_time').reverse()
+	if len(upcomingBookings) > 0:
+		return calculateDifference(time,upcomingBookings[0].start_time)
+	else:
+		return "More than 3:00"
+
+def queryDB(date,time,request):
+	request.session['bk_date'] = date
+	request.session['bk_time'] = time
+	end = datetime.strptime(time,"%H:%M") + timedelta(minutes=15)
+	end = end.strftime("%H:%M")
+	booked_room_ids = bookings.objects.filter(date=date,start_time__lte=time,end_time__gte=end).values_list('room_id',flat=True)
+	avaliable_rooms = rooms.objects.exclude(room_id__in = booked_room_ids)
+	for x in avaliable_rooms:
+		print "ROOM",x.room_id
+		x.room_avaliability = getAvaliabilityTime(x.room_id,date,time)
+	obj = {'query_results': avaliable_rooms, 'bk_date':date, 'bk_time':time}
+	return obj
+
+def getMinutes(request):
+	radio = request.POST.getlist("duration_radio")
+	if radio[0] == "quarter":
+	    return "00:15"
+	elif radio[0] == "half":
+		return "00:30"
+	elif radio[0] == "threequarter":
+		return "00:45"
+	elif radio[0] == "onehour":
+		return "01:00"
+	elif radio[0] == "userDuration":
+		return request.POST['durValue'] 
+
+def convertMinutes(request):
+	minutes = getMinutes(request)
+	hours = minutes[0] + minutes[1]
+	hours = int(hours)
+	minutes1 = minutes[3]+ minutes[4]
+	min = int(minutes1) + 60*hours
+	return minutes,min
+
+def convertDuration(start,end):
+	hours = int(end.split(":")[0]) - int(start.split(":")[0])
+	minutes = int(end.split(":")[1]) - int(start.split(":")[1])
+	if minutes < 0:
+		hours = hours - 1
+		minutes = 60 + minutes
+	elif minutes == 0:
+		minutes = "00"
+	duration = str(hours) + ":" + str(minutes)
+	return duration
+
